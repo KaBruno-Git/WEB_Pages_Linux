2c36240b7ebe2352c50c7585cdb0dafe9f294718
Kauan Bruno <kauanbrunocs@gmail.com>
2026-04-07 10:07:18 -0300

    Create ip.php

---
+++ Pages/ip.php
@@ -0,0 +1,23 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['Logado'])){
+    header("Location: index.php");
+    exit();
+  }
+  $OUT = shell_exec('ip a');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>IP Information</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="card">
+      <h2>IP Information</h2>
+      <pre><?= $OUT ?></pre>
+      <a href="menu.php" class="back">Back to Menu</a>
+    </div>
+  </body>
+</html>
