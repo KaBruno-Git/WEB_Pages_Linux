743a29b60b467693b2d868eab9697231bd12f4fd
Kauan Bruno <kauanbrunocs@gmail.com>
2026-04-07 10:08:37 -0300

    Create menu.php

---
+++ Pages/menu.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if(!isset($_SESSION['Logado'])){
+  header("Location: index.php");
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Menu</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="card">
+      <h2>Welcome to system</h2>
+      <nav>
+        <a href="lscpu.php">CPU Information</a>
+        <a href="lsusb.php">USB List</a>
+        <a href="ip.php">IP Information</a>
+        <a href="systemctl.php">System Information</a>
+        <a href="networkctl.php">Network Information</a>
+        <a href="mudanome.php">Change VM name</a>
+        <a href="logout.php" class="logout">Logout</a>
+      </nav>
+    </div>
+  </body>
+</html>
